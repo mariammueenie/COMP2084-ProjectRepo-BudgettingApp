10aeccfd2df9fd24077541e9f46349ab33c592a8
mariammueenie <156461593+mariammueenie@users.noreply.github.com>
2025-08-13 10:41:10 -0400

    Add google auth to program.cs

--- BudgetingApp/obj/Debug/net8.0/BudgetingApp.AssemblyInfo.cs
+++ BudgetingApp/obj/Debug/net8.0/BudgetingApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba0864192ad172f68479a33a096fbd9f3cee6031")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a311053dba2257439112645a517d479df4ffdf68")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
